677c910c10dcbc69f8cae5142190e304d13951da
Haoxiang Fei <fettes@umich.edu>
2022-12-20 00:59:28 -0500

    fix(build): sim rule

--- make/sim.mk
+++ make/sim.mk
@@ -2,8 +2,18 @@ MACRO += SIM
 
 include make/sim/vcs.mk
 
+SIM_TGT := $(SIM_DIR)/$(SIM_EXE)
+SIM_RUN := $(subst /,_,$(SIM_DIR)/$(SIM_EXE))
+SIM_DIR := $(SIM_DIR)/$(SIM_EXE).d
+
 $(SIM_DIR)/$(SIM_EXE): $(SIM_SRC) | $(SIM_DIR)
 	$(SIM) $(SIM_FLAGS) $^ -o $@
 
+$(SIM_TGT): $(SIM_DIR)/$(SIM_EXE)
+	ln -sf $(SIM_EXE).d/$(SIM_EXE) $@
+
 $(SIM_DIR):
 	mkdir -p $@
+
+$(SIM_RUN): $(SIM_DIR)/$(SIM_EXE)
+	cd $(SIM_DIR) && ./$(SIM_EXE)
