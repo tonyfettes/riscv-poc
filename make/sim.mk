aaed8155d0cd0aa4327fc708e53f6f36a9ce562b
Tony Fettes <tonyfettes@tonyfettes.com>
2022-12-23 00:03:23 -0500

    fix(build): remove the name pollutions of SIM_DIR

--- make/sim.mk
+++ make/sim.mk
@@ -2,18 +2,18 @@ MACRO += SIM
 
 include make/sim/vcs.mk
 
-SIM_TGT := $(SIM_DIR)/$(SIM_EXE)
-SIM_RUN := $(subst /,_,$(SIM_DIR)/$(SIM_EXE))
-SIM_DIR := $(SIM_DIR)/$(SIM_EXE).d
+SIM_TGT = $(SIM_DIR)/$(SIM_EXE)
+SIM_RUN = $(subst /,_,$(SIM_DIR)/$(SIM_EXE))
+SIM_INT = $(SIM_DIR)/$(SIM_EXE).d
 
-$(SIM_DIR)/$(SIM_EXE): $(SIM_SRC) | $(SIM_DIR)
+$(SIM_INT)/$(SIM_EXE): $(SIM_SRC) | $(SIM_INT)
 	$(SIM) $(SIM_FLAGS) $^ -o $@
 
-$(SIM_TGT): $(SIM_DIR)/$(SIM_EXE)
+$(SIM_TGT): $(SIM_INT)/$(SIM_EXE)
 	ln -sf $(SIM_EXE).d/$(SIM_EXE) $@
 
-$(SIM_DIR):
+$(SIM_INT):
 	mkdir -p $@
 
-$(SIM_RUN): $(SIM_DIR)/$(SIM_EXE)
-	cd $(SIM_DIR) && ./$(SIM_EXE)
+$(SIM_RUN): $(SIM_INT)/$(SIM_EXE)
+	cd $(SIM_INT) && ./$(SIM_EXE)
